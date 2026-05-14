5865584a2595ce468bfe6b639f9f619945c45c03
AngelaSapin <katsuremi@gmail.com>
2026-05-14 11:16:15 +0800

    Add files via upload

---
+++ assets/js/javaScript.js
@@ -0,0 +1,20 @@
+
+function redirectToHomeWebsite() {
+    window.location.href = "Home.html";
+}
+
+function redirectToAboutWebsite() {
+    window.location.href = "Valentine.html";
+}
+
+function redirectSkillsWebsite() {
+    window.location.href = "Decide.html";
+}
+
+function redirectToProjectsWebsite() {
+    window.location.href = "yes.html";
+}
+
+function redirectToNoWebsite() {
+    window.location.href = "no.html";
+}
